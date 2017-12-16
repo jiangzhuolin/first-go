84ef139a4adcd623fcbec408397a7fedc2ff7b16
jiangzhuolin <jiang_zhuolin@sina.com>
2017-12-17 01:33:55 +0800

    fix bug for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 # @Author: jiangzhuolin
 # @Date:   2017-12-16 18:58:35
 # @Last Modified by:   jiangzhuolin
-# @Last Modified time: 2017-12-17 01:31:05
+# @Last Modified time: 2017-12-17 01:33:29
 
 #! /bin/sh
 
 DATE=`date +"%Y-%m-%d %H:%M:%S"`
 
 echo "${DATE} | start to restart webserver..." >> ./deploy.log
-kill -9 $(pgrep webserver}
+kill -9 $(pgrep webserver)
 cd /opt/software/golang/first-go
 #git pull https://github.com/jiangzhuolin/first-go.git
 git pull
